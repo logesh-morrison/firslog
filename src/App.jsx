b3f70f1cb360f200171783fae3bdb599ceb94680
Logesh U <logesh.u@xerago.com>
2024-11-13 17:55:08 +0530

    read excel

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import readExcel from "./ReadExcel";
 import "./App.css";
 
 function App() {
@@ -28,7 +27,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
-      <p>logesh was here</p>
+      <ReadExcel />
     </>
   );
 }
---
+++ src/ReadExcel.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import * as XLSX from "xlsx";
+
+function parseExcelData(evt) {
+  const bstr = evt.target.result; // binaryStr
+  const wb = XLSX.read(bstr, { type: "binary" }); // parse: 'binaryStr' to 'workbook'
+  const sheet1 = wb.Sheets[wb.SheetNames[0]]; // get: first worksheet   // TODO: read all sheets
+  // var data = XLSX.utils.sheet_to_json(sheet1, { header: 1 }); // array of arrays
+  const data = XLSX.utils.sheet_to_json(sheet1, { header: 2 }); // array of objects
+  return data;
+}
+
+function readExcelFile(files, setData) {
+  const file = files[0]; // TODO: read all files
+  const reader = new FileReader();
+  reader.onload = (e) => {
+    const data = parseExcelData(e);
+    setData({ fileName: file.name, data });
+  };
+  reader.onerror = function () {
+    console.log(reader.error);
+  };
+  reader.readAsBinaryString(file);
+}
+
+function UploadFile({ setData }) {
+  const handleChange = (e) => {
+    console.log(e.target.files);
+    readExcelFile(e.target.files, setData);
+  };
+  return <input type="file" onChange={handleChange} />;
+}
+
+function downloadExcel(data) {
+  /* starting from this data */
+  // var data = [
+  //   { name: "Jagadeesh", age: 11 },
+  //   { name: "Sangeeth", age: 22 }
+  // ];
+
+  /* generate a worksheet */
+  var ws = XLSX.utils.json_to_sheet(data.data);
+
+  /* add to workbook */
+  var wb = XLSX.utils.book_new();
+  XLSX.utils.book_append_sheet(wb, ws, "Presidents");
+
+  /* write workbook and force a download */
+  XLSX.writeFile(wb, "sheetjs.xlsx");
+}
+
+function ExportExcel({ data }) {
+  return (
+    <button type="button" onClick={() => downloadExcel(data)}>
+      Export Excel
+    </button>
+  );
+}
+
+export default function readExcel() {
+  const [data, setData] = useState(null);
+  return (
+    <div>
+      <UploadFile setData={setData} />
+      <ExportExcel data={data} />
+      <pre>{JSON.stringify(data, null, 2)}</pre>
+    </div>
+  );
+}
